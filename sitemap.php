87f0e51eea3fbf10804ad6b491291432105e660a
Aiglator <rayan.chattaoui@gmail.com>
2025-03-13 14:17:44 +0100

    debug sitemap.xml dynamique pour automatisation de la mise a jour dynamique

--- sitemap.php
+++ sitemap.php
@@ -10,6 +10,7 @@ $pages = [
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
+
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($pages as $url => $priority): ?>
         <url>
@@ -19,4 +20,3 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
         </url>
     <?php endforeach; ?>
 </urlset>
-?>
