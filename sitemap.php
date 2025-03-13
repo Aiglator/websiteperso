54598fdae97fe957c927b09acc91bf590ccc04f3
Aiglator <rayan.chattaoui@gmail.com>
2025-03-13 14:06:44 +0100

    correction

---
+++ sitemap.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/xml; charset=utf-8');
+
+$pages = [
+    '/' => 1.0,
+    '/projet.html' => 0.8,
+    '/blog.html' => 0.8,
+    '/contact.html' => 0.7
+];
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <?php foreach ($pages as $url => $priority): ?>
+        <url>
+            <loc>https://rayanchattaoui.com<?= $url ?></loc>
+            <lastmod><?= date('Y-m-d', filemtime(__DIR__ . $url)) ?></lastmod>
+            <priority><?= $priority ?></priority>
+        </url>
+    <?php endforeach; ?>
+</urlset>
+?>
